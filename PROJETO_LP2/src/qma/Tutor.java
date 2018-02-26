8a2fc0456998d493638c20c4f7354a68920a8f0d
LucasOliveiraF <lucas.oliveira.silva@ccc.ufcg.edu.br>
2018-02-26 10:54:23 -0300

    Modifiquei classe tutor

--- PROJETO_LP2/src/qma/Tutor.java
+++ PROJETO_LP2/src/qma/Tutor.java
@@ -7,27 +7,29 @@ import java.util.Set;
 
 public class Tutor {
 	
-	private String disciplina;
+	private Map<String, Integer> disciplinas;
 	private Map<String, String> horarios;
 	private Set<String> locais;
-	private int proficiencia;
 	private int avaliacao;
 	private int dinheiro;
 	
-	public Tutor(String disciplina, int proficiencia) {
-		
-		if (disciplina.trim().isEmpty() || disciplina == null)
-			throw new IllegalArgumentException();
-		if (proficiencia <= 0) {
-			throw new IndexOutOfBoundsException("Erro na definicao de papel: Proficiencia invalida");
-		}
-		
-		this.disciplina = disciplina;
+	public Tutor() {
+		this.disciplinas = new HashMap<>();
 		this.horarios = new HashMap<>();
 		this.locais = new HashSet<>();
-		this.proficiencia = proficiencia;
 		this.avaliacao = 4;
 		this.dinheiro = 0;
+	}
+	
+	public void cadastraDisciplina(String disciplina, int proficiencia) throws Exception {
+		if (disciplina.trim().isEmpty() || disciplina == null)
+			throw new IllegalArgumentException();
+		if (this.disciplinas.containsKey(disciplina))
+			throw new Exception("Erro na definicao de papel: Ja eh tutor dessa disciplina");
+		if (proficiencia <= 0)
+			throw new IndexOutOfBoundsException("Erro na definicao de papel: Proficiencia invalida");
+		
+		this.disciplinas.put(disciplina, proficiencia);
 	}
 	
 	public void cadastraHorario(String horario, String dia) {
