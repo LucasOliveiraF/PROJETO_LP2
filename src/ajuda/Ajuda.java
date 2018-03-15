94b561cf4a0ea5d70ef3e6a380fde6aa252134f4
RuteFarias <rute.farias@ccc.ufcg.edu.br>
2018-03-15 12:35:02 -0300

    Documentação da Classe Ajuda

--- src/ajuda/Ajuda.java
+++ src/ajuda/Ajuda.java
@@ -1,26 +1,55 @@
 package ajuda;
 
+/**
+ * Representacao de ajuda. Cada aluno pode solicitar ajuda a um tutor de acordo com sua necessidade, 
+ * existe dois tipos de ajuda, ajuda online e ajuda presencial. Ajuda eh a classe pai, ajudaOnline e ajudaPresencial 
+ * herdam de Ajuda.
+ * @author Lucas Oliveira e Rute Farias
+ *
+ */
+
+
 public abstract class Ajuda {
 
 	protected String matrAluno, disciplina;
 	protected String matrTutor;
 	protected boolean avaliado;
 	
+	
+	/**
+	 *Constroi uma ajuda com a verificacao de avaliado iniciando com false. 
+	 */
+	
 	public Ajuda() {
 		this.avaliado = false;
 	}
-	
+		
 	public abstract String pegarTutor();
 	
 	public abstract String getInfoAjuda(String atributo);
+	
+	/**
+	 * Retorna um boolean caso o tutor tenha sido avaliado ou nao
+	 * @return true tutor ja avaliado
+	 * @return false tutor nao avaliado
+	 */
 	
 	public boolean getAvaliado() {
 		return this.avaliado;
 	}
 	
+	/**
+	 * Modidica o atributo avaliado para true, quando um tutor eh avaliado.
+	 */
+	
 	public void setAvaliado() {
 		this.avaliado = true;
 	}
+	
+	/**
+	 * Retorna uma String com a matricula do tutor
+	 * @return matricula do tutor
+	 */
 	
 	public String getMatrTutor() {
 		return matrTutor;
